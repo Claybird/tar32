b05ca5aadc247f64b80e9584f7de811d4055a0a4
Claybird <claybird.without.wing@gmail.com>
2023-10-28 16:43:46 +0900

    moved CAutoFile to util.h

--- src/stdafx.h
+++ src/stdafx.h
@@ -46,36 +46,4 @@
 typedef __int64 size64;
 #endif//size64
 
-class CAutoFile {
-protected:
-	FILE* _fp;
-	std::string _path;
-	CAutoFile(const CAutoFile&) = delete;
-	const CAutoFile& operator=(const CAutoFile&) = delete;
-public:
-	CAutoFile() :_fp(NULL) {}
-	virtual ~CAutoFile() {
-		close();
-	}
-	operator FILE* () { return _fp; }
-	bool is_opened() const { return _fp != NULL; }
-	void close() {
-		if (_fp) {
-			fclose(_fp);
-			_fp = NULL;
-			_path.clear();
-		}
-	}
-	void open(const std::string& fname, const std::string& mode = "rb") {
-		close();
-		_path = fname;
-		auto err = fopen_s(&_fp, fname.c_str(), mode.c_str());
-		if (err == 0 && _fp) {
-			//set buffer size
-			setvbuf(_fp, NULL, _IOFBF, 1024 * 1024);
-		}
-	}
-	const std::string get_path()const { return _path; }
-};
-
 #endif
